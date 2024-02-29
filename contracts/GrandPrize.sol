bc5b5a39d266744394de9f5d35c09124ed8002ac
PhantomOz <faniogor@gmail.com>
2024-02-29 11:08:39 +0100

    This commit added readme-doc and grandprize contract

---
+++ contracts/GrandPrize.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/// @title Grand Prize
+/// @author Favour Aniogor
+/// @notice This is responsible for selecting random winners and dissing out airdrop
+/// @dev This uses chainlink VRF to generate random numbers and select winners
+contract GrandPrize {
+
+    
+
+}
